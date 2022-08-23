c0e8755c7673b3b89b4b322b56c41dc30df04c99
khayym <xeyyam.kerimov@gmail.com>
2022-08-23 17:54:06 +0400

    loading

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,23 @@ import AppProvider from '../src/provider/AppProvider'
 // eslint-disable-next-line
 import "swiper/css/bundle";
 import { useEffect, useState } from "react";
+import { Loader } from '@mantine/core';
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
+    setLoading(false);
   }, []);
 
   return (
     <AppProvider>
-      {loading ? <Component {...pageProps} /> : <div style={{ height: '100%', width: '100%', backgroundColor: '#000' }}>Loading...</div>}
+      {
+        loading ? <div style={{ width: '100%', height: '100%', overflow: 'hidden', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'absolute', zIndex: 999999, backgroundColor: '#232323' }}>
+          <Loader color="#A91111" size="xl" variant="bars" />
+        </div>
+          : <Component {...pageProps} />
+      }
     </AppProvider>
   )
 }
