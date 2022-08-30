41f345cd5bb59849d40b41e6ae102f6ef663f92b
khayym <xeyyam.kerimov@gmail.com>
2022-08-30 12:53:29 +0400

    exprimental

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
-  },
   // experimental: { esmExternals: true },
 }
 module.exports = nextConfig
